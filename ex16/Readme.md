2783d339a8422158d30bfc847de43396ea69b09b
Ali Ilgim Demirel <ilgimdemirel@gmail.com>
2017-11-09 20:31:54 +0100

    Finalized ex16, it was easy anyways.

---
+++ ex16/Readme.md
@@ -0,0 +1,15 @@
+# Pointers, Dreaded pointers 
+
+## What we learn;
+* Structs
+* malloc / free
+* assert
+* strdup (string duplicate)
+
+
+From the book:
+>### Explaining Structures
+>A structure in C is a collection of other data types(variables) that are stored in one block of memory where you can access each variable indepentendtly by __NAME__
+
+
+## other findings;
--- ex16/ex16.c
+++ ex16/ex16.c
@@ -43,6 +43,14 @@ void Person_print(struct Person *who)
 	printf("\tWeight: %d\n", who->weight);
 }
 
+// Extra Credit 3, takes in a person as a struct not as a pointer to a struct
+// having said that should check what if I send a ptr to a struct 
+void getStructAsAWhole(struct Person sahis)
+{
+	printf("sizeof Person:%s is :%lu\n", sahis.name, sizeof(sahis));
+
+}
+
 int main(int argc, char *argv[])
 {
 	// make two people structures
@@ -51,23 +59,36 @@ int main(int argc, char *argv[])
 
 	//print them out and where they are in memory
 	printf("Where is joe: %p.\n", joe);
+	printf("What is the sizeof joe:%lu\n", sizeof(*joe));
 	Person_print(joe);
 
 	printf("Where is frank: %p.\n", frank);
+	printf("What is the sizeof frank:%lu\n", sizeof(*frank));
 	Person_print(frank);
 
 	// make everyone age 20 years and print them again
-	joe->age = 20;
+	joe->age += 20;
 	Person_print(joe);
 
-	frank->age = 20;
+	frank->age += 20;
 	Person_print(frank);
 
 	Person_destroy(joe);
-
+	
+	// Create a Person struct named hasan.
 	struct Person hasan = {.name = "Hasan", .age = 30 };
     printf("Who is hasan ? hasan is %s, and hasan is %d years old.\n", hasan.name, hasan.age);
     printf("Lets crash hasan !!!! hasan weights %d.\n", hasan.weight);
+	
+	printf("---\n");
+
+	// check if sending a struct directly works or not.
+	getStructAsAWhole(hasan);
+
+	/*
+	 * // Lets see if we can send a ptr to struct rather than a struct. (Hint; No.)
+	 * getStructAsAWhole(joe);
+     */
 
 	return 0;
 }
